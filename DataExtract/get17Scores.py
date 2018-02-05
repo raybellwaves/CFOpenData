48aa149512acb6d235db1d524fcd3c7ffac09139
Ray Bell <rjbell1987@gmail.com>
2018-02-04 21:19:07 -0500

    Update get17Scores.py

--- DataExtract/get17Scores.py
+++ DataExtract/get17Scores.py
@@ -13,12 +13,12 @@ import time
 start_time = time.time()
     
 def getProfilesFromFile(score_filename, prof_filename, div):
-    score_columns=['Id', 'Name', 'Division', 'OverallRank', 'Rank', 'Wk1_Score', 'Wk1_Rank', 
+    score_columns=['userid', 'Name', 'Division', 'OverallRank', 'OverallScore', 'Wk1_Score', 'Wk1_Rank', 
              'Wk2_Score', 'Wk2_Rank', 'Wk3_Score', 'Wk3_Rank', 'Wk4_Score', 'Wk4_Rank', 'Wk5_Score', 'Wk5_Rank']
     #all the ids of athletes who registered
     dataList = pandas.read_csv(score_filename, names=score_columns, encoding='ISO-8859-1')
-    Id_list = dataList.Id.tolist()
-    print("Total number of Ids " + str(len(Id_list)))
+    Id_list = dataList.userid.tolist()
+    print("Total number of userids " + str(len(Id_list)))
     
     #all of the ids of athlete profiles already downloaded
     prof_columns=('Id', 'Name', 'Affiliate', 'Age', 'Height', 'Weight', 'Sprint 400m', 
@@ -47,9 +47,9 @@ def main():
     #getProfilesFromFile(r'Scores\16_Scores_Men.csv', r'Profiles\Profile_Men.csv', divisions[1])
         
     #*******************#
-    #Get Athlete Scores and Profiles from Interwebz
+    #Get Athlete Scores and Profiles from MainSite
     CFOpenData = extractScoresMainSite.extractScoresMainSite(div,scal,year,numberperpage)
-       #getProfile.getProfile(CFOpenData.Id_list, div, False)
+    #getProfile.getProfile(CFOpenData.Id_list, div, False)
     print("script took " + str((time.time() - start_time) * 60.0) + "minutes)
     
 if __name__ == '__main__':
