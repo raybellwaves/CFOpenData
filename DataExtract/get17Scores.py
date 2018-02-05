3079d74df3ff93c5d1a6b44707c3c3416bdd0e64
Ray Bell <rjbell1987@gmail.com>
2018-02-04 22:12:15 -0500

    Update get17Scores.py

--- DataExtract/get17Scores.py
+++ DataExtract/get17Scores.py
@@ -12,25 +12,25 @@ import pandas
 import time
 start_time = time.time()
     
-def getProfilesFromFile(score_filename, prof_filename, div):
-    score_columns=['userid', 'Name', 'Division', 'OverallRank', 'OverallScore', 'Wk1_Score', 'Wk1_Rank', 
-             'Wk2_Score', 'Wk2_Rank', 'Wk3_Score', 'Wk3_Rank', 'Wk4_Score', 'Wk4_Rank', 'Wk5_Score', 'Wk5_Rank']
-    #all the ids of athletes who registered
-    dataList = pandas.read_csv(score_filename, names=score_columns, encoding='ISO-8859-1')
-    Id_list = dataList.userid.tolist()
-    print("Total number of userids " + str(len(Id_list)))
-    
-    #all of the ids of athlete profiles already downloaded
-    prof_columns=('Id', 'Name', 'Affiliate', 'Age', 'Height', 'Weight', 'Sprint 400m', 
-                               'Clean & Jerk', 'Snatch', 'Deadlift', 'Back Squat', 'Max Pull-ups')
-    profileList = pandas.read_csv(prof_filename, names=prof_columns, encoding='ISO-8859-1')
-    remove_list = profileList.Id.tolist() #get the names from the profile list
-    for item in remove_list: #if a name is in the profile list and in the scores list then it has already downloaded
-        if item in Id_list:
-            del Id_list[Id_list.index(item)] #so remove the id from the list of profiles to download
-    
-    print("Number of Ids to download " + str(len(Id_list)))
-    getProfile.getProfile(Id_list,div,True)
+#def getProfilesFromFile(score_filename, prof_filename, div):
+#    score_columns=['userid', 'Name', 'Division', 'OverallRank', 'OverallScore', 'Wk1_Score', 'Wk1_Rank', 
+#             'Wk2_Score', 'Wk2_Rank', 'Wk3_Score', 'Wk3_Rank', 'Wk4_Score', 'Wk4_Rank', 'Wk5_Score', 'Wk5_Rank']
+#    #all the ids of athletes who registered
+#    dataList = pandas.read_csv(score_filename, names=score_columns, encoding='ISO-8859-1')
+#    Id_list = dataList.userid.tolist()
+#    print("Total number of userids " + str(len(Id_list)))
+#    
+#    #all of the ids of athlete profiles already downloaded
+#    prof_columns=('Id', 'Name', 'Affiliate', 'Age', 'Height', 'Weight', 'Sprint 400m', 
+#                               'Clean & Jerk', 'Snatch', 'Deadlift', 'Back Squat', 'Max Pull-ups')
+#    profileList = pandas.read_csv(prof_filename, names=prof_columns, encoding='ISO-8859-1')
+#    remove_list = profileList.Id.tolist() #get the names from the profile list
+#    for item in remove_list: #if a name is in the profile list and in the scores list then it has already downloaded
+#        if item in Id_list:
+#            del Id_list[Id_list.index(item)] #so remove the id from the list of profiles to download
+#    
+#    print("Number of Ids to download " + str(len(Id_list)))
+#    getProfile.getProfile(Id_list,div,True)
 
 def main():
     logging.basicConfig(filename='async.log',format='%(asctime)s %(message)s',level=logging.DEBUG)
